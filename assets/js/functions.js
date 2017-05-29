76f600cda049c39a610f62d73fb82f87262e0b5d
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-05-29 08:32:58 -0300

    Ajustes nos negritos e scroll formulario

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,3 +13,13 @@ for (var i = 0; i < dropdowns.length; i++) {
     }
   });
 }
+
+var scroll = document.querySelector('.action');
+var input  = document.querySelector('input[name="data[name]"]');
+
+scroll.addEventListener('click', function(event) {
+  document.body.scrollTop = document.body.scrollHeight;
+  input.focus();
+
+  event.preventDefault();
+});
