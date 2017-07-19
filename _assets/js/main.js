8286a307ca1bb2fbf174111a5b73945b1a51a757
Cleiton Souza <cleiton7souza@gmail.com>
2017-07-19 18:24:07 -0300

    questionario

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -39,3 +39,55 @@ if (typeof trigger != 'undefined' && trigger.length && typeof input != 'undefine
     }
   });
 }
+
+/**
+ * Custom radio
+ */
+$('.custom-radio').on('click', function(event) {
+  event.preventDefault();
+
+  var obj = $(event.target);
+  var parent = obj.closest('ul');
+
+  if (parent == 'undefined') {
+    return;
+  }
+
+  var rel = parent.attr('rel');
+
+  if (!rel) {
+    return;
+  }
+
+  var hidden = $('input[rel="' + rel + '"]');
+
+  if (typeof hidden == 'undefined' || !hidden.length) {
+    return;
+  }
+
+  hidden.val(obj.parent().text());
+
+  parent.find('.custom-radio.active').removeClass('active');
+  obj.addClass('active');
+});
+
+/**
+ * Trigger list
+ */
+$('.trigger-list').on('click', 'li', function(event) {
+  $(event.target).find('.custom-radio').trigger('click');
+});
+
+$('*[data-query]').each(function() {
+  var query = $(this).data('query');
+  var url = new URL(window.location.href);
+  url = url.searchParams.get(query);
+
+  $(this).val(url);
+
+  var redirect = $('[name="data[redirect_url]"]');
+
+  if (typeof redirect !== 'undefined') {
+    redirect.val(redirect.val() + '?' + query + '=' + url);
+  }
+});
